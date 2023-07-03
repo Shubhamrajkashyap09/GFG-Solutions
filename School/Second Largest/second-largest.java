c8086e341213417dadaf65c13946302f546c3547
Shubham Raj Kashyap <shubhamrajkashyap09@gmail.com>
2023-07-03 23:31:32 +0530

    Added Solution - GfG to GitHub

---
+++ School/Second Largest/second-largest.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().print2largest(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int print2largest(int a[], int n) {
+        // code here
+        int max1=a[0];
+       int max2=-1;
+       int i;
+       for(i=1;i<n;i++)
+       {
+           if(a[i]>max1)
+           {
+               max2=max1;
+               max1=a[i];
+           }
+           else if(a[i]<max1 && a[i]>max2)
+               max2=a[i];
+       }
+       return max2;
+    }
+}
