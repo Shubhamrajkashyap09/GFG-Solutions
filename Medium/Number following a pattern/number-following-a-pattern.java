62ac8950ef675d857c90a91526ce943de71d7856
Shubham Raj Kashyap <shubhamrajkashyap09@gmail.com>
2023-11-10 22:06:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number following a pattern/number-following-a-pattern.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.printMinNumberForPattern(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String printMinNumberForPattern(String s){
+        // code here
+         int n= s.length();
+       int count =1;
+       char[] ans= new char[n+1];
+       for(int i=0;i<=n;i++){
+           if(i == n || s.charAt(i) == 'I'){
+               
+               for(int j=i-1 ; j>=-1;j--){
+                   ans[j+1]= (char)('0'+count);
+                   count++;
+                   if( j>=0 && s.charAt(j)  == 'I')
+                   break;
+               }
+           }
+       }
+       return new String(ans);
+    }
+}
